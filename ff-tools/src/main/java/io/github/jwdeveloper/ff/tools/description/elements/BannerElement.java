2748f4e5ac42293961196972817894d8b77f2477
JW <jacekwoln@gmail.com>
2023-09-12 12:09:11 +0200

    ..

--- ff-tools/src/main/java/io/github/jwdeveloper/ff/tools/description/elements/BannerElement.java
+++ ff-tools/src/main/java/io/github/jwdeveloper/ff/tools/description/elements/BannerElement.java
@@ -33,10 +33,10 @@ public class BannerElement implements DescriptionDecorator {
 
 
 
-        var supportLink = factory.imageElement(path + "/discord_medium.svg", bannerOptions.getDonationUrl());
-        var discordLink = factory.imageElement(path + "/discord_medium.svg", bannerOptions.getDiscordUrl());
-        var githubLink = factory.imageElement(path + "/discord_medium.svg", bannerOptions.getGithubUrl());
-        var spigotLink = factory.imageElement(path + "/discord_medium.svg", bannerOptions.getSpigotUrl());
+        var supportLink = factory.imageElement(path + "/discord_medium2.svg", bannerOptions.getDonationUrl());
+        var discordLink = factory.imageElement(path + "/discord_medium2.svg", bannerOptions.getDiscordUrl());
+        var githubLink = factory.imageElement(path + "/discord_medium2.svg", bannerOptions.getGithubUrl());
+        var spigotLink = factory.imageElement(path + "/discord_medium2.svg", bannerOptions.getSpigotUrl());
 
         discordLink.setProperty("width", "30%");
         supportLink.setProperty("width", "30%");
